19a7401f426b773c67b6e3cb5e700f3b7baf8ab8
Anatoly Loyko <Anatoly.Loyko@ringcentral.com>
2024-12-06 12:29:59 +0400

    AWS-000 init project

---
+++ app/src/main/kotlin/App.kt
@@ -0,0 +1,11 @@
+package by.anatolyloyko.app
+
+import by.anatolyloyko.utils.Printer
+
+// This is the main entry point of the application.
+// It uses the `Printer` class from the `:utils` subproject.
+fun main() {
+    val message = "Hello JetBrains!"
+    val printer = Printer(message)
+    printer.printMessage()
+}
