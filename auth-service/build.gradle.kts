ee668a508d23ec94956d3f38f5cd5b6fc9542797
tvoe-zdorovje <dr0sha98@gmail.com>
2025-05-22 13:47:09 +0400

    AUS-6627366 init a new module

---
+++ auth-service/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+group = "by.anatolyloyko.ams"
+version = "unspecified"
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+val jdkVersion: Int = (project.findProperty("jdkVersion") as String).toInt()
+
+kotlin {
+    jvmToolchain(jdkVersion)
+}
+
+val databaseDriver: String by project
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-logging")
+    implementation("org.springframework.boot:spring-boot-starter-graphql")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jooq")
+
+    implementation(databaseDriver)
+}
+
+tasks.withType<BootJar> {
+    enabled = false
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
