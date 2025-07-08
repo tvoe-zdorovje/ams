b59ea5fa8f28b0b0663628b89450cac251f1ea2b
tvoe-zdorovje <dr0sha98@gmail.com>
2025-07-08 13:41:20 +0400

    fix kdoc

--- studio-service/src/main/kotlin/by/anatolyloyko/ams/studio/action/SaveStudioAction.kt
+++ studio-service/src/main/kotlin/by/anatolyloyko/ams/studio/action/SaveStudioAction.kt
@@ -3,14 +3,14 @@ package by.anatolyloyko.ams.studio.action
 import by.anatolyloyko.ams.studio.model.Studio
 
 /**
- * Action responsible for creating a new studio.
+ * Action responsible for creating a new studio or updating an existing one.
  */
 interface SaveStudioAction {
     /**
-     * Creates a new studio and returns the generated ID.
+     * Saves a studio and returns its ID.
      *
      * @param studio the studio data.
-     * @return the ID of the newly created studio.
+     * @return the ID of the saved studio.
      */
     operator fun invoke(studio: Studio): Long
 }
