cec415611ae83fddf1132680e418c8617b4e4bc0
Anatoli Loika <dr0sha98@gmail.com>
2026-07-24 16:58:10 +0300

    GTW-6580020 init gateway-service

---
+++ gateway/gateway-service/build.gradle.kts
@@ -0,0 +1,45 @@
+group = "by.anatolyloyko.ams"
+version = "0.2.0"
+
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+val jdkVersion: Int = (project.findProperty("jdkVersion") as String).toInt()
+
+kotlin {
+    jvmToolchain(jdkVersion)
+}
+
+dependencies {
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.cloud:spring-cloud-starter-circuitbreaker-reactor-resilience4j")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+val springCloudVersion: String by project
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
